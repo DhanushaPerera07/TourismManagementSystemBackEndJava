a3b4c70642c9d112c668a09555678c329c500c37
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-12-12 16:41:32 +0530

    Test EmployeeDAO save() operation.

---
+++ src/test/java/com/elephasvacation/tms/web/dal/custom/impl/EmployeeDAOImplTest.java
@@ -0,0 +1,107 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Dhanusha Perera
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.elephasvacation.tms.web.dal.custom.impl;
+
+import com.elephasvacation.tms.web.dal.DAOFactory;
+import com.elephasvacation.tms.web.dal.DAOTypes;
+import com.elephasvacation.tms.web.dal.custom.EmployeeDAO;
+import com.elephasvacation.tms.web.entity.Employee;
+import com.elephasvacation.tms.web.entity.enumeration.GenderTypes;
+import com.elephasvacation.tms.web.util.HibernateUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertNotNull;
+
+public class EmployeeDAOImplTest {
+
+    private final EmployeeDAO employeeDAO = DAOFactory.getInstance().getDAO(DAOTypes.EMPLOYEE);
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    @Before
+    public void setUp() {
+        try {
+            /* get EntityManagerFactory. */
+            this.emf = HibernateUtil.getEntityManagerFactory();
+            /* creates EntityManager. */
+            this.em = emf.createEntityManager();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        /* close the EntityManagerFactory and EntityManager. */
+        if (em != null) {
+            em.close();
+            emf.close();
+        }
+    }
+
+    @Test
+    public void save() {
+        try {
+            /* begins the transaction. */
+            this.em.getTransaction().begin();
+
+            /* set EntityManager. */
+            this.employeeDAO.setEntityManager(this.em);
+
+            /* creates a new Employee object. */
+            Employee kusal = new Employee("John Doe",
+                    "New York",
+                    LocalDate.of(1991, 01, 01),
+                    "112233445566",
+                    "03321234567",
+                    "john.test@gmail.com",
+                    GenderTypes.MALE,
+                    "Trainee",
+                    "Active");
+
+            /* saving the Employee. */
+            Integer generatedEmployeeId = this.employeeDAO.save(kusal);
+
+            /* assert */
+            assertNotNull(generatedEmployeeId);
+
+            /* print the generated ID on the terminal. */
+            System.out.println("Generated Employee ID: " + generatedEmployeeId);
+
+            /* committing the transaction. */
+            this.em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
