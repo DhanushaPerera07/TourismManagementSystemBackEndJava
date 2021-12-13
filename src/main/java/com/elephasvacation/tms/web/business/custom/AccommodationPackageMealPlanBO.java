d40b5b700c12b911e929f4cd7e7b39bbaad6a530
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-12-14 01:59:42 +0530

    Change the methods' return types instead of using Integer, and format the code

--- src/main/java/com/elephasvacation/tms/web/business/custom/AccommodationPackageMealPlanBO.java
+++ src/main/java/com/elephasvacation/tms/web/business/custom/AccommodationPackageMealPlanBO.java
@@ -15,7 +15,7 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -32,12 +32,15 @@ import java.util.List;
 
 public interface AccommodationPackageMealPlanBO extends SuperBO {
 
-    Integer addAccommodationPackageMealPlan(AccommodationPackageMealPlanDTO accommodationPackageMealPlanDTO) throws Exception;
+    AccommodationPackageMealPlanDTO
+    addAccommodationPackageMealPlan(AccommodationPackageMealPlanDTO accommodationPackageMealPlanDTO)
+            throws Exception;
 
 //    void updateMealPlan(AccommodationPackageMealPlanDTO accommodationPackageMealPlanDTO,
 //                        AccommodationPackageMealPlanDTO newAccommodationPackageMealPlanDTO) throws Exception;
 
-    void deleteAccommodationPackageMealPlan(AccommodationPackageMealPlanDTO accommodationPackageMealPlanDTO) throws Exception;
+    void deleteAccommodationPackageMealPlan(AccommodationPackageMealPlanDTO accommodationPackageMealPlanDTO)
+            throws Exception;
 
     MealPlanDTO getAccommodationPackageMealPlan(AccommodationPackageMealPlanDTO accommodationPackageMealPlanDTO)
             throws Exception;
