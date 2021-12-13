33ddd32d410481c97a07ccf11d827550fef86bf7
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-12-14 01:53:43 +0530

    Change the Integer ID to AccommodationRateDTOId, and Fix typos

--- src/main/java/com/elephasvacation/tms/web/business/custom/AccommodationRateBO.java
+++ src/main/java/com/elephasvacation/tms/web/business/custom/AccommodationRateBO.java
@@ -1,22 +1,47 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Dhanusha Perera
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.elephasvacation.tms.web.business.custom;
 
 import com.elephasvacation.tms.web.business.SuperBO;
 import com.elephasvacation.tms.web.dto.AccommodationRateDTO;
+import com.elephasvacation.tms.web.dto.AccommodationRateDTOId;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface AccommodationRateBO extends SuperBO {
 
-    Integer createAccommodationRate(AccommodationRateDTO accommodationRateDTO) throws Exception;
+    AccommodationRateDTOId createAccommodationRate(AccommodationRateDTO accommodationRateDTO) throws Exception;
 
     void updateAccommodationRate(AccommodationRateDTO accommodationRateDTO) throws Exception;
 
-    void deleteAccommodationRate(Integer accommodationRateDTOID) throws Exception;
+    void deleteAccommodationRate(AccommodationRateDTOId accommodationRateDTOId) throws Exception;
 
-    AccommodationRateDTO getAccommodationRateByID(Integer accommodationRateDTOID) throws Exception;
+    AccommodationRateDTO getAccommodationRateByID(AccommodationRateDTOId accommodationRateDTOId) throws Exception;
 
     List<AccommodationRateDTO> getAllAccommodationRates(Integer accommodationRateDTOID) throws Exception;
 
-    List<AccommodationRateDTO> getAllAccommodationRatesForAccommodationPackage(Integer accommodationPackageID) throws SQLException;
+    List<AccommodationRateDTO> getAllAccommodationRatesForAccommodationPackage(Integer accommodationPackageID)
+            throws SQLException;
 }
