8fedebf40f94f4d2331ab53576bb1b7a18089c7e
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-12-25 18:49:38 +0530

    Change the Logger related log messages according to the changes made in constant files.

--- src/main/java/com/elephasvacation/tms/web/util/LogConfig.java
+++ src/main/java/com/elephasvacation/tms/web/util/LogConfig.java
@@ -1,30 +1,26 @@
-/**
+/*
  * MIT License
- * <p>
+ *
  * Copyright (c) 2021 Dhanusha Perera
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * @author : Dhanusha Perera
- * @since : 21/04/2021
- */
-/**
  * @author : Dhanusha Perera
  * @since : 21/04/2021
  */
@@ -45,7 +41,6 @@ public class LogConfig {
 
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LogConfig.class);
 
-
     public static void initLogging() {
         createLoggingPath();
 
@@ -54,14 +49,13 @@ public class LogConfig {
             fileHandler.setFormatter(new SimpleFormatter());
             fileHandler.setLevel(Level.INFO);
             Logger.getLogger("").addHandler(fileHandler);
-            logger.info(SuccessfulMessages.LOGGER_INITIALIZED_SUCCESSFUL);
+            logger.info(SuccessfulMessages.ServletContext.LOGGER_INITIALIZED_SUCCESSFULLY);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
     }
-
 
     public static void createLoggingPath() {
         File file = new File(Commons.LOGGER_FILE_FULL_PATH);
@@ -76,7 +70,6 @@ public class LogConfig {
             }
         }
     }
-
 
     private static boolean createAFile(File file) throws IOException {
         return file.createNewFile();
