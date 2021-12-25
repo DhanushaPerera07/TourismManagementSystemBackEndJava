6868273bab3cd1ad82953376bd31b2666b222709
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-12-25 18:51:43 +0530

    Initialize AppInitializer.java in contextInitialized() method.

--- src/main/java/com/elephasvacation/tms/web/listener/MyContextListener.java
+++ src/main/java/com/elephasvacation/tms/web/listener/MyContextListener.java
@@ -1,21 +1,21 @@
-/**
+/*
  * MIT License
- * <p>
+ *
  * Copyright (c) 2021 Dhanusha Perera
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -24,12 +24,10 @@
  * @author : Dhanusha Perera
  * @since : 21/04/2021
  */
-/**
- * @author : Dhanusha Perera
- * @since : 21/04/2021
- */
 package com.elephasvacation.tms.web.listener;
 
+import com.elephasvacation.tms.web.AppInitializer;
+import com.elephasvacation.tms.web.commonConstant.FailedMessages;
 import com.elephasvacation.tms.web.commonConstant.HibernateConstant;
 import com.elephasvacation.tms.web.commonConstant.SuccessfulMessages;
 import com.elephasvacation.tms.web.util.HibernateUtil;
@@ -55,10 +53,18 @@ public class MyContextListener implements ServletContextListener {
         /* This method is called when the servlet context is initialized
         (when the Web application is deployed). */
         LogConfig.initLogging();
-        logger.info(SuccessfulMessages.CONTEXT_INITIALIZED_SUCCESSFUL);
+
+        /* Initialize AppInitializer.java */
+        try {
+            Class.forName(AppInitializer.class.getName());
+        } catch (ClassNotFoundException e) {
+            logger.error(FailedMessages.Spring.FAILED_LOADING_SPRING_CONTAINER, e);
+        }
+
         EntityManagerFactory entityManagerFactory = HibernateUtil.getEntityManagerFactory();
         // let's set an attribute for EntityManagerFactory, and pass the EMF object.
         sce.getServletContext().setAttribute(HibernateConstant.ENTITY_MANAGER_FACTORY, entityManagerFactory);
+        logger.info(SuccessfulMessages.ServletContext.CONTEXT_INITIALIZED_SUCCESSFULLY);
 
     }
 
