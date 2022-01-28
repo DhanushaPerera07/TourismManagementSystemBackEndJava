d1a0450ae8b75954e838b078072a92d6c41ae524
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-01-28 16:30:57 +0530

    Config app initializer.

--- src/main/java/com/elephasvacation/tms/web/WebAppInitializer.java
+++ src/main/java/com/elephasvacation/tms/web/WebAppInitializer.java
@@ -1,2 +1,31 @@
-package com.elephasvacation.tms.web;public class WebAppInitializer {
+package com.elephasvacation.tms.web;
+
+import com.elephasvacation.tms.web.config.LogConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    public WebAppInitializer() {
+        /* Initialize the App Logger. */
+        LogConfig.initLogging();
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                WebRootConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+                WebAppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
