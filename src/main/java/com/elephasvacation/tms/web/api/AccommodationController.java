4af663fe43c5b21eb115d2be67a6cfb6dc2c858a
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-02-06 19:44:24 +0530

    Implement the basic CRUD methods and mappings.

--- src/main/java/com/elephasvacation/tms/web/api/AccommodationController.java
+++ src/main/java/com/elephasvacation/tms/web/api/AccommodationController.java
@@ -27,13 +27,93 @@
 */
 package com.elephasvacation.tms.web.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.elephasvacation.tms.web.api.util.ApiUtil;
+import com.elephasvacation.tms.web.business.custom.AccommodationBO;
+import com.elephasvacation.tms.web.commonconstant.API;
+import com.elephasvacation.tms.web.dto.AccommodationDTO;
+import com.elephasvacation.tms.web.exception.IdFormatException;
+import com.elephasvacation.tms.web.exception.RecordNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+import java.util.List;
+
+
+@CrossOrigin(origins = API.HTTP_LOCALHOST_8080)
 @RequestMapping("/api/v1/accommodations")
 @RestController
 public class AccommodationController {
+    @Autowired
+    private AccommodationBO accommodationBO;
 
+    /**
+     * Get all accommodations list.
+     *
+     * @return List<AccommodationDTO> accommodationsList.
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<AccommodationDTO> getAllAccommodations() {
+        return this.accommodationBO.getAllAccommodations();
+    }
+
+    /**
+     * Get accommodation by accommodation ID.
+     *
+     * @return AccommodationDTO accommodation object.
+     * @throws IdFormatException       if the ID is not an Integer.
+     * @throws RecordNotFoundException if matching accommodation record not found,
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+            value = "/{id}")
+    public AccommodationDTO getAccommodationByID(@PathVariable(name = "id") String id) {
+        System.out.println("AccommodationID: " + id);
+
+        Integer accommodationID = ApiUtil.getIntegerId(id);
+
+        AccommodationDTO accommodationDTO = this.accommodationBO.getAccommodationByID(accommodationID);
+        System.out.println("accommodationDTO Result: " + accommodationDTO);
+
+        /* If no matching accommodation found. */
+        if (accommodationDTO == null) throw new RecordNotFoundException();
+        return accommodationDTO;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Integer saveAccommodation(@RequestBody AccommodationDTO accommodationDTO) {
+        System.out.println("API Layer: AccommodationDTO ---> " + accommodationDTO);
+        return this.accommodationBO.createAccommodation(accommodationDTO);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateAccommodation(@PathVariable String id,
+                                    @RequestBody AccommodationDTO accommodationDTO) {
+        Integer accommodationID = ApiUtil.getIntegerId(id);
+
+        /* TODO: Accommodation - update validation logic. */
+        if (accommodationDTO.getId() == accommodationID) {
+            accommodationDTO.setId(accommodationID);
+            this.accommodationBO.updateAccommodation(accommodationDTO);
+        } else {
+            /* URL param accommodationID and accommodationObject's accommodationID mismatched.
+            Therefore, Let's throw an error.*/
+            /* TODO: handle error. */
+            throw new RuntimeException();
+        }
+
+    }
+
+    /**
+     * Delete accommodation by Accommodation ID.
+     */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public void deleteAccommodation(@PathVariable String id) {
+        Integer accommodationID = ApiUtil.getIntegerId(id);
+        System.out.println("Accommodation ID: " + accommodationID);
+        accommodationBO.deleteAccommodation(accommodationID);
+    }
 }
