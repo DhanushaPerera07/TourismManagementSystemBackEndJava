c06887a3bac3b77a199319fdb540230f753297b0
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-02-06 22:28:37 +0530

    Add missing @RestController annotation and Implement the basic CRUD methods and mappings.

--- src/main/java/com/elephasvacation/tms/web/api/RoomTypeController.java
+++ src/main/java/com/elephasvacation/tms/web/api/RoomTypeController.java
@@ -27,11 +27,94 @@
 */
 package com.elephasvacation.tms.web.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.elephasvacation.tms.web.api.util.ApiUtil;
+import com.elephasvacation.tms.web.business.custom.RoomTypeBO;
+import com.elephasvacation.tms.web.commonconstant.API;
+import com.elephasvacation.tms.web.dto.RoomTypeDTO;
+import com.elephasvacation.tms.web.exception.IdFormatException;
+import com.elephasvacation.tms.web.exception.RecordNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+import java.util.List;
+
+@CrossOrigin(origins = API.HTTP_LOCALHOST_8080)
 @RequestMapping("/api/v1/room-types")
+@RestController
 public class RoomTypeController {
+
+    @Autowired
+    private RoomTypeBO roomTypeBO;
+
+    /**
+     * Get all roomTypes list.
+     *
+     * @return List<RoomTypeDTO> roomTypesList.
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RoomTypeDTO> getAllRoomTypes() {
+        return this.roomTypeBO.getAllRoomTypes();
+    }
+
+    /**
+     * Get roomType by roomType ID.
+     *
+     * @return RoomTypeDTO roomType object.
+     * @throws IdFormatException       if the ID is not an Integer.
+     * @throws RecordNotFoundException if matching roomType record not found,
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+            value = "/{id}")
+    public RoomTypeDTO getRoomTypeByID(@PathVariable(name = "id") String id) {
+        System.out.println("RoomTypeID: " + id);
+
+        Integer roomTypeID = ApiUtil.getIntegerId(id);
+
+        RoomTypeDTO roomTypeDTO = this.roomTypeBO.getRoomTypeByID(roomTypeID);
+        System.out.println("RoomType Result: " + roomTypeDTO);
+
+        /* If RoomType not found. */
+        if (roomTypeDTO == null) throw new RecordNotFoundException();
+        return roomTypeDTO;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Integer saveRoomType(@RequestBody RoomTypeDTO roomTypeDTO) {
+        System.out.println("API Layer: RoomTypeDTO ---> " + roomTypeDTO);
+        return this.roomTypeBO.createRoomType(roomTypeDTO);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateRoomType(@PathVariable String id,
+                               @RequestBody RoomTypeDTO roomTypeDTO) {
+        Integer roomTypeID = ApiUtil.getIntegerId(id);
+
+        /* TODO: RoomType - update validation logic. */
+        if (roomTypeDTO.getId() == roomTypeID) {
+            roomTypeDTO.setId(roomTypeID);
+            this.roomTypeBO.updateRoomType(roomTypeDTO);
+        } else {
+            /* URL param roomTypeID and roomTypeObject's roomTypeID mismatched.
+            Therefore, Let's throw an error.*/
+            /* TODO: handle error. */
+            throw new RuntimeException();
+        }
+
+    }
+
+    /**
+     * Delete roomType by RoomTypeID.
+     */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public void deleteRoomType(@PathVariable String id) {
+        Integer roomTypeID = ApiUtil.getIntegerId(id);
+        System.out.println("RoomType ID: " + roomTypeID);
+        this.roomTypeBO.deleteRoomType(roomTypeID);
+    }
 
 }
