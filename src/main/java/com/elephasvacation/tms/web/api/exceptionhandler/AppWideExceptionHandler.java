20b03e92d3601e147e29772583e300249e787d90
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-02-17 12:59:50 +0530

    Handle exceptions and return meaningful and simple error messages.

--- src/main/java/com/elephasvacation/tms/web/api/exceptionhandler/AppWideExceptionHandler.java
+++ src/main/java/com/elephasvacation/tms/web/api/exceptionhandler/AppWideExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.elephasvacation.tms.web.api.exceptionhandler;
 
+import com.elephasvacation.tms.web.exception.BadRequestException;
+import com.elephasvacation.tms.web.exception.DateTimeInvalidException;
 import com.elephasvacation.tms.web.exception.IdFormatException;
 import com.elephasvacation.tms.web.exception.RecordNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,22 +23,35 @@ public class AppWideExceptionHandler {
      * HTTP response will NOT contain HTML response body (it just contains the `reason`).
      */
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IdFormatException.class)
+    @ExceptionHandler({IdFormatException.class})
     public String handleIdNotValid() {
         return "ID is not valid.";
     }
 
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    @ExceptionHandler(value = {RecordNotFoundException.class, NoSuchElementException.class})
-    public String recordNotFoundException() {
-        return "Record not found.";
+    @ExceptionHandler(value = {RecordNotFoundException.class, NoSuchElementException.class, EmptyResultDataAccessException.class})
+    public String recordNotFoundException(RecordNotFoundException recordNotFoundException) {
+        return (recordNotFoundException == null) ? "Record not found." : "Record not found: " +
+                recordNotFoundException.getMessage();
     }
 
 //    @ResponseStatus(code = HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NoSuchElementException.class)
+//    @ExceptionHandler(value = {EmptyResultDataAccessException.class})
 //    public String NoMatchingRecordNotFoundException() {
 //        return "Record not found.";
 //    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public String BadRequestException(BadRequestException badRequestException) {
+        return (badRequestException == null) ? "Invalid data." : "Invalid data: " + badRequestException.getMessage();
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DateTimeInvalidException.class)
+    public String DateTimeInvalidException() {
+        return "Provided DateTime is invalid.";
+    }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Throwable.class)
